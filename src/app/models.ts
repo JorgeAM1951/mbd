d7d26724f0ef92d0fadfee746221fdc439e4db60
JorgeAM1951 <jamroccat@gmail.com>
2022-05-08 21:36:37 +0100

    minor tweaks

--- src/app/models.ts
+++ src/app/models.ts
@@ -1,8 +1,6 @@
 export interface Card{
     card_image: string;
     name: string;
-    mana_cost: string;
-    mana_value: number;
     colors: Array<string>;
     description: string;
     stats: string;
